dd358ee7ced8d019488ee18121cc2fafb7185a08
celuj <jota_elpapa@hotmail.com>
2026-08-25 15:49:28 -0500

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,3 @@
+const firebaseConfig={apiKey:"AIzaSyAixwAxV-QNxgzn-qCKMb3C4gdHEajV08E",authDomain:"rifas-celuj.firebaseapp.com",projectId:"rifas-celuj",storageBucket:"rifas-celuj.firebasestorage.app",messagingSenderId:"810770275870",appId:"1:810770275870:web:8a19000163796f8d45980f",measurementId:"G-Q3NQLBFXHM"};
+if(!firebase.apps.length)firebase.initializeApp(firebaseConfig);
+const db=firebase.firestore();const auth=firebase.auth();const ADMIN_UID="FhVg6EpradNuZhUd63Cd4grkLbk2";
